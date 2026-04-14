298fc1076c0f6edf360db5d87c6ebb0ee7a9a86f
sonalrmeshram <sonalrmeshram@gmail.com>
2026-04-14 15:29:37 +0530

    Update db.js

--- backend/configs/db.js
+++ backend/configs/db.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-   host: '34.55.43.79',
-   port: '3306',
+   host: '127.0.0.1',     // ← Changed from 34.55.43.79
+   port: 3306,
    user: 'sonal',
    password: 'Sonal@45815',
    database: 'react_node_app'
