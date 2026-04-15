4cc85d8d79059c0b2ba975fe965be1847d30ec3d
sonal45815 <sonal45815@gmail.com>
2026-04-15 11:22:01 +0530

    Update AuthorsController.js

--- backend/controllers/AuthorsController.js
+++ backend/controllers/AuthorsController.js
@@ -1,119 +1,112 @@
 const db = require('../configs/db');
 
-function AuthorsController() { }
+function AuthorsController() {}
 
 const getQuery = 'SELECT * FROM author';
 
-AuthorsController.prototype.get = async (req, res) => {
-   try {
+AuthorsController.prototype.get = (req, res) => {
+  db.query(getQuery, (err, authors) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({
+        message: 'Error executing query.',
+      });
+    }
+
+    return res.status(200).json({
+      authors,
+    });
+  });
+};
+
+AuthorsController.prototype.create = (req, res) => {
+  const { name, birthday, bio } = req.body;
+
+  db.query(
+    'INSERT INTO author (name, birthday, bio, createdAt, updatedAt) VALUES (?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)',
+    [name, birthday, bio],
+    (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({
+          message: 'Error executing query.',
+        });
+      }
+
       db.query(getQuery, (err, authors) => {
-         if (err) {
-            throw new Error("Error executing query.");
-         }
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            message: 'Error executing query.',
+          });
+        }
 
-         res.status(200).json({
-            authors: authors,
-         });
+        return res.status(200).json({
+          message: 'Author created successfully!',
+          authors,
+        });
       });
-   } catch (error) {
-      console.error(error);
-      res.status(500).json({
-         message:
-            "Something unexpected has happened. Please try again later.",
-      });
-   }
+    }
+  );
 };
 
-AuthorsController.prototype.create = async (req, res) => {
-   try {
-      const { name, birthday, bio } = req.body;
+AuthorsController.prototype.update = (req, res) => {
+  const authorId = req.params.id;
+  const { name, birthday, bio } = req.body;
 
-      db.query('INSERT INTO author (name, birthday, bio, createdAt, updatedAt) VALUES (?, ?, ?, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP)', [
-         name, new Date(birthday), bio], (err) => {
-            if (err) {
-               console.log(err);
-               throw new Error("Error executing query.");
-            }
+  db.query(
+    'UPDATE author SET name = ?, birthday = ?, bio = ?, updatedAt = CURRENT_TIMESTAMP WHERE id = ?',
+    [name, birthday, bio, authorId],
+    (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({
+          message: 'Error executing query.',
+        });
+      }
 
-            db.query(getQuery, (err, authors) => {
-               if (err) {
-                  throw new Error("Error executing query.");
-               }
+      db.query(getQuery, (err, authors) => {
+        if (err) {
+          console.error(err);
+          return res.status(500).json({
+            message: 'Error executing query.',
+          });
+        }
 
-               return res.status(200).json({
-                  message: `Author created successfully!`,
-                  authors: authors,
-               });
-            });
-         });
-   } catch (error) {
-      console.error(error);
-      res.status(500).json({
-         message:
-            "Something unexpected has happened. Please try again later.",
+        return res.status(200).json({
+          message: 'Author updated successfully!',
+          authors,
+        });
       });
-   }
+    }
+  );
 };
 
-AuthorsController.prototype.update = async (req, res) => {
-   try {
-      const authorId = req.params.id;
-      const { name, birthday, bio } = req.body;
+AuthorsController.prototype.delete = (req, res) => {
+  const authorId = req.params.id;
 
-      db.query(`UPDATE author SET name = ?, birthday = ?, bio = ?, updatedAt = CURRENT_TIMESTAMP WHERE id = ?`, [
-         name, new Date(birthday), bio, authorId], (err) => {
-            if (err) {
-               console.log(err);
-               throw new Error("Error executing query.");
-            }
-
-            db.query(getQuery, (err, authors) => {
-               if (err) {
-                  throw new Error("Error executing query.");
-               }
-
-               return res.status(200).json({
-                  message: `Author updated successfully!`,
-                  authors: authors,
-               });
-            });
-         });
-   } catch (error) {
-      console.error(error);
-      res.status(500).json({
-         message:
-            "Something unexpected has happened. Please try again later.",
+  db.query('DELETE FROM author WHERE id = ?', [authorId], (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({
+        message: 'Error executing query.',
       });
-   }
-};
+    }
 
-AuthorsController.prototype.delete = async (req, res) => {
-   try {
-      const authorId = req.params.id;
+    db.query(getQuery, (err, authors) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({
+          message: 'Error executing query.',
+        });
+      }
 
-      db.query('DELETE FROM author WHERE id = ?', [authorId], (err, result) => {
-         if (err) {
-            throw new Error("Error executing query.");
-         }
-
-         db.query(getQuery, (err, authors) => {
-            if (err) {
-               throw new Error("Error executing query.");
-            }
-
-            return res.status(200).json({
-               message: `Author deleted successfully!`,
-               authors: authors,
-            });
-         });
+      return res.status(200).json({
+        message: 'Author deleted successfully!',
+        authors,
       });
-   } catch (error) {
-      console.error(error);
-      res.status(500).json({
-         message:
-            "Something unexpected has happened. Please try again later.",
-      });
-   }
+    });
+  });
 };
 
 module.exports = new AuthorsController();
